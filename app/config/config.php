f567e766d03a8f6b1294a66e56e600ef9ff0f50d
Vic <143060326+Vixxxzy@users.noreply.github.com>
2025-05-06 20:54:45 +0800

    adding MediAppointment Website

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	// define path constants
+    define("APP_PATH", "http://localhost/MediAppointment/public");
+
+    // define database config
+    define("HOST_DB", "localhost");
+    define("NAME_DB", "appointment");
+    define("USER_DB", "root");
+    define("PASS_DB", "12345");
+	
+	
+?>
