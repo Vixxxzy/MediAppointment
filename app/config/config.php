4e729f5f1979067b9db2036e4266f42de9aabd23
Vic <143060326+Vixxxzy@users.noreply.github.com>
2025-05-06 21:25:50 +0800

    update config again

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 	// define path constants
-    define("APP_PATH", "http://103.23.199.190/MediAppointment/MediAppointment/public");
+    define("APP_PATH", "http://103.23.199.190/MediAppointment/public");
 
     // define database config
     define("HOST_DB", "localhost");
