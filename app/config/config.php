3312f388c545964be832f57e41ebd9d1d8131f02
Vic <143060326+Vixxxzy@users.noreply.github.com>
2025-05-07 08:08:27 +0800

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 	// define path constants
-    define("APP_PATH", "https://mediappointment.site/public");
+    define("APP_PATH", "https://mediappointment.site");
 
     // define database config
     define("HOST_DB", "localhost");
